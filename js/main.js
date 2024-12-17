2bffd45f96824c6e7e5f4e200aa54e57a48945d9
alvin-odins <alvin.odins@gmail.com>
2024-12-17 21:15:34 +0100

    conditionals to show either photo or video

--- js/main.js
+++ js/main.js
@@ -8,22 +8,30 @@ function getFetch(){
     alert('Please choose a date before generating photo of day')
     return;
   }
-  
+
   const url = `https://api.nasa.gov/planetary/apod?api_key=OZxdntAjPsj7i2DLWnNP4tuQyKDdzKvN10i0C0Lr&date=${choice}`
 
   fetch(url)
       .then(res => res.json()) // parse response as JSON
       .then(data => {
         console.log(data)
-        if(data.media_type == 'image'){
+
+        if(data.media_type === 'image'){
+
           document.querySelector('img').src = data.hdurl
-        } else if(data.media_type == 'video') {
+          document.querySelector('img').classList.remove('hidden')
+          document.querySelector('iframe').classList.add('hidden')
+        } else if(data.media_type === 'video') {
+
           document.querySelector('iframe').src = data.url
+          document.querySelector('iframe').classList.remove('hidden')
+          document.querySelector('iframe').classList.add('hidden')
         }
       
         document.querySelector('h3').innerText = data.explanation
       })
       .catch(err => {
           console.log(`error ${err}`)
+          alert('Unexpected delays being encountered. Please try again later')
       });
 }
